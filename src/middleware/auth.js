01a9f4f50aa30caf1ee00c71ce970edb498fe323
Jayantc <35648900+Jayantc@users.noreply.github.com>
2020-06-30 00:01:24 +0530

    Update auth.js

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,15 +3,9 @@ const User= require('../models/user')
 
 const auth= async(req, res, next)=>{
     try{
-        console.log('hi')
         const token= req.header('Authorization').replace('Bearer ','')
-        console.log('hi')
-        console.log(token)
-        console.log(process.env.JWT_SECRET)
         const decoded= jwt.verify(token, process.env.JWT_SECRET)
-        console.log('hi')
         const user= await User.findOne({_id: decoded._id, 'tokens.token':token})
-        console.log('hi')
 
         if(!user){
             throw new Error()
